3ee726bd4f964a0076e93a1bcdcb245fd1e49b1c
toughtiger <santosh.cerdak@gmail.com>
2021-01-16 09:03:11 +0530

    Added graphql schema and resolver

---
+++ schema/schema.js
@@ -0,0 +1,36 @@
+const graphql = require("graphql");
+const _ = require('lodash');
+const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+
+let books = [
+    {name: "Ramayan", genere: "Dharmik", id: "01"},
+    {name: "Mahabharat", genere: "Dharmik", id: "02"},
+    {name: "Geeta", genere: "Dharmik", id: "03"},
+]
+
+const BookType = new GraphQLObjectType({
+  name: "Book",
+  fields: () => ({
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    genere: { type: GraphQLString },
+  }),
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: {
+    book: {
+      type: BookType,
+      args: { id: { type: GraphQLString } },
+      resolve(parent, args) {
+        // code to query DB/ file
+       return _.find(books,{id: args.id})
+      },
+    },
+  },
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery,
+});
